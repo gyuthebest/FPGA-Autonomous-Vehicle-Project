7b31a915ae7d12a160cd815c773f10e3c16d52d6
gyuthebest <kkim297064@gmail.com>
2026-07-30 16:49:48 +0900

    Add sensor reliability and checker modules

---
+++ sensor_checker.sv
@@ -0,0 +1,225 @@
+//sensor_checker- 주석 + 수정본
+//abs 안 받고 안에서 계산하게 설계하
+
+
+`timescale 1ns / 1ps
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+//signed_check_module
+//signed, unsinged 둘다 별로 상관 없을 듯
+//WIDTH - sensor data 폭, THRESHOLD는 각각 range check 시 범위, USE_MIN, USE_MAX는 min, max 중 안 쓰는게 있으면 0으로 지정하여 기능을 끄도록 설계
+//clk, rst_n 은 필요 없을 수 있는데 일단 모든 모듈의 통일성을 위해 넣어둠, value는 받는 센서 측정값에 해당
+module range_check_signed #(
+    parameter WIDTH = 12,
+    parameter THRESHOLD_MAX = 100,
+    parameter THRESHOLD_MIN = 100,
+    parameter USE_MIN = 1,
+    parameter USE_MAX = 1
+)( 
+    input logic clk,
+    input logic rst_n,
+    input logic signed [WIDTH-1:0] value, // 수정: signed
+    output logic out_in_range
+    );
+//기준 이상 밑 이하의 값이 나올 경우 out_in_range, USE_MIN,USE_MAX와 AND 처리 되어있어서 0이 될시 자동적으로 if 문 안이 0이됨
+    always_comb begin
+    out_in_range = 0;
+
+    if (USE_MIN && (value < THRESHOLD_MIN))
+        out_in_range = 1;
+
+    if (USE_MAX && (value > THRESHOLD_MAX))
+        out_in_range = 1;
+end
+endmodule
+
+
+// jump check 의 경우에는 과거와 현재 값을 둘 다 사용할 수 있어야한다. ->즉 register가 필요하기 때문에 다른 모듈과의 timing에 대해서는 생각해보는것도 나쁘지 않을지도
+
+module jump_check #(
+    parameter WIDTH = 12,
+    parameter THRESHOLD = 100
+)(
+    input logic clk,
+    input logic rst_n,
+    input logic signed [WIDTH-1:0] current_data,
+    input logic temp_except, //온도 예외처리용. 1이면 jump check 무조건 이상 없는것
+    output logic jump_error
+    );
+///////////////////////////////////////////////////////////  
+//prev_data는 기존 값, diff는 절댓값에 해당한다. 아래는 diff, prev_data 정의한것
+ logic signed [WIDTH-1:0] prev_data; 
+ logic signed [WIDTH-1:0] diff; 
+ 
+    always_ff @(posedge clk) begin //수정: 동기리셋
+        if(!rst_n) prev_data <= '0; 
+        else prev_data <= current_data; 
+    end 
+        
+    always_comb begin if (current_data >= prev_data) 
+        diff = current_data - prev_data; 
+        else diff = prev_data - current_data; 
+    end 
+
+    //절댓값이 기준값보다 작으면 jump_error=0. temp_except = 1(날씨 전환되는 시점)이면 무조건 0
+    always_comb begin 
+        if (temp_except)
+           jump_error = 0;
+        else if (diff < THRESHOLD ) 
+	       jump_error =0; 
+        else jump_error = 1; 
+    end 
+endmodule
+
+//stuck check
+//사실 HISTORY는 뺴도 되긴하다 만약 HISTORY 길이를 전부 10비트로 맞출거라면
+//new_sample 받아와서 신호가 들어왔음을 인지해야한다
+module stuck_check #(
+    parameter WIDTH=16,
+    parameter HISTORY =10,
+    parameter THRESHOLD=10
+)(
+    input logic clk,
+    input logic rst_n,
+    input logic new_sample,
+    input logic signed [WIDTH-1:0] current_data,
+    input logic check_enable,
+    output logic is_stuck
+    );
+    logic signed [WIDTH-1:0] prev_data;
+    logic [HISTORY-1:0] stuck_history;
+    logic signed [WIDTH-1:0] diff;
+    logic [$clog2(HISTORY+1)-1:0] stuck_count;
+    
+    
+    always_comb begin 
+        if (current_data >= prev_data) 
+        diff = current_data - prev_data; 
+    else 
+        diff = prev_data - current_data; 
+    end 
+    //우선 reset, 그후 new sample이 들어올때 stuck history 는 left shift 하면서, (diff < THRESHOLD)&&check_enable가 true면 1, 아니면 0을 LSB에 채운다. 그후 prev_data를 업데이트
+    //즉 stuck_history의 경우 기존 10개의 값에 대해 check를 진행하게 됨, 그중 특정 개수만큼이 1이면 stuck 으로 판단
+    //이때 check_enable의 경우에는 조건식이 온다. 예를 들어 distance != 20000이면 distance 이 20000인 경우에는 자동적으로 0이되게 하던가 혹은 weather delta !=0 이면 날씨가 바뀔 때 1이 되게하는 등 
+    //조건을 만족하게
+    always_ff @(posedge clk) begin //수정: 동기 리셋
+    if (!rst_n) begin 
+    prev_data <= '0; 
+    stuck_history <= '0; 
+    end 
+    else if (new_sample) begin 
+    stuck_history <= { stuck_history[HISTORY-2:0], (diff < THRESHOLD)&&check_enable}; 
+    prev_data <= current_data; 
+    end 
+    end 
+
+    //stuck_count의 경우 최근 HISTORY만큼의 데이터 중 몇개가 stuck으로 count되었는지 확인하는것
+    always_comb begin 
+	stuck_count = 0; 
+        for (int i = 0; i < HISTORY; i++) begin 
+        stuck_count = stuck_count + stuck_history[i]; 
+        end 
+    end 
+    //최종적으로 is_stuck 판단 여부 (stuck 8회 이상 되었을 때 is_stuck가 됨)
+    always_comb begin 
+    is_stuck = (stuck_count >= 8); 
+    end 
+endmodule
+
+//time_out check
+//UPDATE_PERIOD는 따로 받아와야한다(업데이트 주기값을 알아서 와야함)
+//COUNTERWIDTH의 경우 history와 유사한 역할
+module timeout_check #(
+    parameter UPDATE_PERIOD = 10, // 나중에 결정될 값
+    parameter COUNTERWIDTH =10
+    )(
+    input logic clk,
+    input logic rst_n,
+    input  logic valid,
+    output logic is_time_out
+    );
+    logic [COUNTERWIDTH-1:0] count;
+//1클락 마다 counter가 증가하는데 valid 들어오면 0으로 리셋. 즉 만약 clock 이 업데이트주기의 2배 이상 돌아 count 값이 커졌는데 리셋이 안됨 --> valid 값이 입력되지 않고 있음
+always_ff @(posedge clk) begin // 수정: 동기리셋
+    if (!rst_n  )
+        count <= 0;
+    else if(valid)
+        count <=0;
+   else
+        count <= count + 1;
+end
+    always_comb begin
+    is_time_out = (count >= 2*UPDATE_PERIOD);
+    end
+endmodule
+
+
+//noise_check
+//실제 current 값을 받을 필요 없고 마찬가지로 history만 필요함-->new_sample만 받으면 될듯
+module noise_check #(
+    parameter WIDTH =16,
+    parameter HISTORY =10
+)(
+    input logic clk,
+    input logic rst_n,
+    input logic jump_error,
+    input logic new_sample,
+    output logic noise_high
+    );
+    logic [HISTORY-1:0] noise_history;
+    logic [$clog2(HISTORY+1)-1:0] noise_count;
+    
+    //마찬가지로 newsample 받아오면 LSB에 jump_error 값이 들어옴
+    //이때 jump_error 는 위의 jump_error 모듈에서 logic으로 빼와서 받아와야함
+    always_ff @(posedge clk) begin // 수정: 동기리셋
+    if (!rst_n) begin
+       noise_history <= '0;
+      end
+    else if (new_sample) begin
+        noise_history <= {
+            noise_history[HISTORY-2:0],jump_error};
+    end
+    end
+//아까 stuck check 의 논리와 유사함    
+always_comb begin
+    noise_count = 0;
+
+    for (int i = 0; i < HISTORY; i++) begin
+        noise_count = noise_count + noise_history[i];
+    end
+end
+always_comb begin
+    noise_high = (noise_count >= 4);
+end
+
+endmodule
+   
+// 여기서부터는 temperature, voltage 특정 범위에 따라 warning 뜨게 하는 모듈에 해당한다
+ //temperature_warn, voltage_warn
+module temp_voltage_checker(
+    input logic clk,
+    input logic rst_n,
+    input logic signed [11:0] temperature,
+    input logic [7:0] voltage,
+
+    output logic temperature_warn,
+    output logic voltage_warn
+    );
+    
+always_comb begin
+    temperature_warn = (temperature <= -200) || (temperature >= 500); // 추가
+    voltage_warn = (voltage < 112); // 추가
+end
+
+endmodule
+
+//timing 여부가 맞는지는 확인하지 못함
+//temp, vlotage의 경우 구체적인 수치는 다시 바꿔놔야함 특히 11.2 처럼 소수점 어떻게 할지
+//속도 기준은 아직 짜지 못함
+//온도의 jumpcheck의 경우에는 불러온 모듈 밖에서 코드를 하나 더 만들어야할듯
+//sensor_reliability output에 뭐 연결할지 못정함
+//input에 abs 지우지 못함
+//속도 모듈 아직 없음
+//아마 값이나 이런거 틀린거 있울 수 있고, 모듈 input output에 연결한 변수명, 모듈 불러온 인스턴스명 잘 확인해야할듯(제가 복붙 시킨거라 변수명이 안바뀌었을수도 있습니다..)
---
+++ sensor_reliability.sv
@@ -0,0 +1,691 @@
+`timescale 1ns / 1ps
+
+//output package pkg에서 묶어놔야할듯, output port 정의해줘야함
+//current value가 [WIDHT-1:0]으로 정의 됨
+//delta의 abs를 넣어야할 것임 ㄴㄴ
+//noise check 할 때 jump_error 값 필요 --> logic으로 따로 정의해줘야함 
+//new_sample 어디서 받는지... 그리고 sensor input을 preprocessed랑 같이 받을 수 있는 timing이 되는지도 확인해줘야함ㅁ
+//stuck check 변화가 있으 때의 조건이 필요할 시 모듈로 가서 input delta를 받아와서 if delta>0 일 때의 조건으로 바꾸면 될듯
+
+//수정: 조도, 온도, 습도, 전압에 대해서는 stuck check 하지 않음. 고정된게 정상인 값이기 때문
+
+import types_pkg::*;
+
+module Sensor_reiability(
+    input logic clk,
+    input logic rst_n,
+    input logic valid, // 추가
+    input sensor_data_T sensor_data_in, 
+    input logic [7:0] Speed, // Speed 어디로 오는지 확인해야함
+    
+    output logic out_in_range_distance, jump_error_distance, is_stuck_distance, noise_high_distance, // output 추가
+    output logic out_in_range_appspeed, jump_error_appspeed, is_stuck_appspeed, noise_high_appspeed,
+    output logic out_in_range_accel_x, jump_error_accel_x, is_stuck_accel_x, noise_high_accel_x,
+    output logic out_in_range_accel_y, jump_error_accel_y, is_stuck_accel_y, noise_high_accel_y,
+    output logic out_in_range_accel_z, jump_error_accel_z, is_stuck_accel_z, noise_high_accel_z,
+    output logic out_in_range_gyro_x, jump_error_gyro_x, is_stuck_gyro_x, noise_high_gyro_x,
+    output logic out_in_range_gyro_y, jump_error_gyro_y, is_stuck_gyro_y, noise_high_gyro_y,
+    output logic out_in_range_gyro_z, jump_error_gyro_z, is_stuck_gyro_z, noise_high_gyro_z,
+    output logic out_in_range_lux, jump_error_lux, noise_high_lux,
+    output logic out_in_range_temp, jump_error_temp, noise_high_temp,
+    output logic out_in_range_hum, jump_error_hum, noise_high_hum,
+    output logic out_in_range_voltage, jump_error_voltage, noise_high_voltage,
+
+    output logic is_timeout_distance, is_timeout_appspeed,
+    output logic is_timeout_accel_x, is_timeout_accel_y, is_timeout_accel_z,
+    output logic is_timeout_gyro_x, is_timeout_gyro_y, is_timeout_gyro_z,
+    output logic is_timeout_lux, is_timeout_temp, is_timeout_hum, is_timeout_voltage,
+
+    output logic temperature_warn,
+    output logic voltage_warn
+    );
+  
+    
+///distance reliability
+//거리 stuck check 애매함 
+
+    range_check_signed #(
+    .WIDTH (16),
+    .THRESHOLD_MAX(20000),
+    .THRESHOLD_MIN(0),
+    .USE_MIN(0),
+    .USE_MAX(1)
+    )u_range_distance(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.distance),
+    .out_in_range(out_in_range_distance)
+);
+   
+    jump_check #(
+    .WIDTH(16),
+    .THRESHOLD(400)
+)u_jump_check_distance(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.distance),
+    .temp_except(1'b0), // 수정: abs 대신 temp_except
+    .jump_error(jump_error_distance)
+    );
+    
+    // 거리 Stuck check 따로 만들기
+    logic [7:0] prev_speed;
+    
+    always_ff @(posedge clk) begin
+        if(!rst_n)
+            prev_speed <= 8'd0;
+        else if (valid)
+            prev_speed <= Speed;
+    end
+    
+    logic speed_change; //차량 속도 변화하는지
+    assign speed_change = (Speed != prev_speed); //sensor_data_in에 Speed 있는지 확인 필요
+    
+    logic distance_stuck_check_en;
+    assign distance_stuck_check_en = (sensor_data_in.distance != 16'd20000) && speed_change;
+    
+    
+    
+    stuck_check #(
+    .WIDTH(16),
+    .HISTORY (10),
+    .THRESHOLD(1)
+)u_stuck_check_distance(  //수정: 거리 신뢰도 판단에 대한 로직 적용
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.distance),
+    .new_sample(valid),
+    .check_enable(distance_stuck_check_en),
+    .is_stuck(is_stuck_distance) // 수정: abs 삭제
+    );
+    
+     noise_check #(
+    .WIDTH(16),
+    .HISTORY(10)
+)u_noise_check_distance(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_distance),
+    .new_sample(valid),
+    .noise_high(noise_high_distance)
+    );
+
+// 접근속도 reliability. 의문: 확인필요
+    range_check_signed #(
+        .WIDTH(16), 
+        .THRESHOLD_MAX(278), 
+        .THRESHOLD_MIN(-278), 
+        .USE_MIN(1), 
+        .USE_MAX(1)
+        )u_range_appspeed (
+        .clk(clk), 
+        .rst_n(rst_n), 
+        .value(sensor_data_in.appspeed), 
+        .out_in_range(out_in_range_appspeed)
+    );
+
+    jump_check #(
+        .WIDTH(16), 
+        .THRESHOLD(200) //시뮬레이션 통해 확인 필요
+    )u_jump_appspeed (
+        .clk(clk), 
+        .rst_n(rst_n), 
+        .current_data(sensor_data_in.appspeed), 
+        .temp_except(1'b0), 
+        .jump_error(jump_error_appspeed)
+    );
+
+    stuck_check #(
+        .WIDTH(16), 
+        .HISTORY(10), 
+        .THRESHOLD(1)
+    )u_stuck_appspeed (
+        .clk(clk), 
+        .rst_n(rst_n), 
+        .new_sample(valid), 
+        .current_data(sensor_data_in.appspeed), // 새 신호 안 만들고 갖다쓴거임.
+        .check_enable(distance_stuck_check_en), 
+        .is_stuck(is_stuck_appspeed)
+    );
+
+    noise_check #(
+        .WIDTH(16), 
+        .HISTORY(10)
+    )u_noise_appspeed (
+        .clk(clk), 
+        .rst_n(rst_n), 
+        .jump_error(jump_error_appspeed), 
+        .new_sample(valid), 
+        .noise_high(noise_high_appspeed)
+    );
+
+
+
+//Accel_x reliability    
+
+    range_check_signed #(
+    .WIDTH (16),
+    .THRESHOLD_MAX(16384),
+    .THRESHOLD_MIN(-16384),
+    .USE_MIN(1),
+    .USE_MAX(1)
+    )u_range_accel_x(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.accel_x),
+    .out_in_range(out_in_range_accel_x)
+);
+   
+    jump_check #(
+    .WIDTH(16),
+    .THRESHOLD(8192)
+)u_jump_accel_x(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.accel_x),
+    .temp_except(1'b0),
+    .jump_error(jump_error_accel_x)
+    );
+    
+    stuck_check #(
+    .WIDTH(16),
+    .HISTORY (10),
+    .THRESHOLD(232)
+)u_stuck_accel_x(
+    .clk(clk),
+    .rst_n(rst_n),
+    .new_sample(valid),
+    .current_data(sensor_data_in.accel_x),
+    .check_enable(speed_change),
+    .is_stuck(is_stuck_accel_x)
+    );
+    
+     noise_check #(
+    .WIDTH(16),
+    .HISTORY(10)
+)u_noise_accel_x(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_accel_x),
+    .new_sample(valid),
+    .noise_high(noise_high_accel_x)
+    );
+
+ //accel_y check
+    logic [15:0] prev_gyro_z;
+    logic direction_change;
+    
+    always_ff @(posedge clk) begin           //로직 추가
+        if(!rst_n)
+            prev_gyro_z <= 16'd0;
+        else if (valid)
+            prev_gyro_z <= sensor_data_in.gyro_z;
+    end
+    
+    assign direction_change = (sensor_data_in.gyro_z != prev_gyro_z);
+
+    range_check_signed #(
+    .WIDTH (16),
+    .THRESHOLD_MAX(16384),
+    .THRESHOLD_MIN(-16384),
+    .USE_MIN(1),
+    .USE_MAX(1)
+    )u_range_accel_y(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.accel_y),
+    .out_in_range(out_in_range_accel_y)
+);
+   
+    jump_check #(
+    .WIDTH(16),
+    .THRESHOLD(8192)
+)u_jump_accel_y(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.accel_y),
+    .temp_except(1'b0),
+    .jump_error(jump_error_accel_y)
+    );
+    
+    stuck_check #(
+    .WIDTH(16),
+    .HISTORY (10),
+    .THRESHOLD(24)
+)u_stuck_accel_y(
+    .clk(clk),
+    .rst_n(rst_n),
+    .new_sample(valid),
+    .current_data(sensor_data_in.accel_y),
+    .check_enable(direction_change),
+    .is_stuck(is_stuck_accel_y)
+    );
+    
+     noise_check #(
+    .WIDTH(16),
+    .HISTORY(10)
+)u_noise_accel_y(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_accel_y),
+    .new_sample(valid),
+    .noise_high(noise_high_accel_y)
+    );
+    
+ //accel_z 
+    logic accel_z_stuck_check_en; // 로직 만들어주기
+    assign accel_z_stuck_check_en = (Speed != 8'b0);
+    
+    
+    range_check_signed #(
+    .WIDTH (16),
+    .THRESHOLD_MAX(16384),
+    .THRESHOLD_MIN(-16384),
+    .USE_MIN(1),
+    .USE_MAX(1)
+    )u_range_accel_z(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.accel_z),
+    .out_in_range(out_in_range_accel_z)
+);
+   
+    jump_check #(
+    .WIDTH(16),
+    .THRESHOLD(8192)
+)u_jump_accel_z(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.accel_z),
+    .temp_except(1'b0),
+    .jump_error(jump_error_accel_z)
+    );
+    
+    stuck_check #( //속도가 0이 아닐때인 경우 반영해.
+    .WIDTH(16),
+    .HISTORY (10),
+    .THRESHOLD(20)
+)u_stuck_accel_z(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.accel_z),
+    .new_sample(valid),
+    .check_enable (accel_z_stuck_check_en),
+    .is_stuck(is_stuck_accel_z)
+    );
+    
+     noise_check #(
+    .WIDTH(16),
+    .HISTORY(10)
+)u_noise_accel_z(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_accel_z),
+    .new_sample(valid),
+    .noise_high(noise_high_accel_z)
+    );
+    
+ //gyro_x
+
+    range_check_signed #(
+    .WIDTH (16),
+    .THRESHOLD_MAX(20480),
+    .THRESHOLD_MIN(-20480),
+    .USE_MIN(1),
+    .USE_MAX(1)
+    )u_range_gyro_x(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.gyro_x),
+    .out_in_range(out_in_range_gyro_x)
+);
+   
+    jump_check #(
+    .WIDTH(16),
+    .THRESHOLD(10240)
+)u_jump_gyro_x(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.gyro_x),
+    .temp_except(1'b0),
+    .jump_error(jump_error_gyro_x)
+    );
+    
+    stuck_check #(
+    .WIDTH(16),
+    .HISTORY (10),
+    .THRESHOLD(50)
+)u_stuck_gyro_x(
+    .clk(clk),
+    .rst_n(rst_n),
+    .new_sample(valid),
+    .current_data(sensor_data_in.gyro_x),
+    .check_enable(direction_change),
+    .is_stuck(is_stuck_gyro_x)
+    );
+    
+     noise_check #(
+    .WIDTH(16),
+    .HISTORY(10)
+)u_noise_gyro_x(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_gyro_x),
+    .new_sample(valid),
+    .noise_high(noise_high_gyro_x)
+    );
+    
+ //gyro_y   
+
+    range_check_signed #(
+    .WIDTH (16),
+    .THRESHOLD_MAX(15360),
+    .THRESHOLD_MIN(-15360),
+    .USE_MIN(1),
+    .USE_MAX(1)
+    )u_range_gyro_y(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.gyro_y),
+    .out_in_range(out_in_range_gyro_y)
+);
+   
+    jump_check #(
+    .WIDTH(16),
+    .THRESHOLD(7680)
+)u_jump_gyro_y(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.gyro_y),
+    .temp_except(1'b0),
+    .jump_error(jump_error_gyro_y)
+    );
+    
+    stuck_check #(
+    .WIDTH(16),
+    .HISTORY (10),
+    .THRESHOLD(20)
+)u_stuck_gyro_y(
+    .clk(clk),
+    .rst_n(rst_n),
+    .new_sample(valid),
+     .current_data(sensor_data_in.gyro_y),
+    .check_enable(speed_change),
+    .is_stuck(is_stuck_gyro_y)
+    );
+    
+     noise_check #(
+    .WIDTH(16),
+    .HISTORY(10)
+)u_noise_gyro_y(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_gyro_y),
+    .new_sample(valid),
+    .noise_high(noise_high_gyro_y)
+    );
+    
+//gyro_z
+
+    range_check_signed #(
+    .WIDTH (16),
+    .THRESHOLD_MAX(30720),
+    .THRESHOLD_MIN(-30720),
+    .USE_MIN(1),
+    .USE_MAX(1)
+    )u_range_gyro_z(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.gyro_z),
+    .out_in_range(out_in_range_gyro_z)
+);
+   
+    jump_check #(
+    .WIDTH(16),
+    .THRESHOLD(15360)
+)u_jump_gyro_z(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.gyro_z),
+    .temp_except(1'b0),
+    .jump_error(jump_error_gyro_z)
+    );
+    
+    stuck_check #(
+    .WIDTH(16),
+    .HISTORY (10),
+    .THRESHOLD(16)
+)u_stuck_gyro_z(
+    .clk(clk),
+    .rst_n(rst_n),
+    .new_sample(valid),
+    .current_data(sensor_data_in.gyro_z),
+    .check_enable(direction_change),
+    .is_stuck(is_stuck_gyro_z)
+    );
+    
+     noise_check #(
+    .WIDTH(16),
+    .HISTORY(10)
+)u_noise_gyro_z(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_gyro_z),
+    .new_sample(valid),
+    .noise_high(noise_high_gyro_z)
+    );
+    
+ //lux
+
+    range_check_signed #(
+    .WIDTH (17),
+    .THRESHOLD_MAX(30720),
+    .THRESHOLD_MIN(10),
+    .USE_MIN(1),
+    .USE_MAX(0)
+    )u_range_lux(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.lux),
+    .out_in_range(out_in_range_lux)
+);
+   
+    jump_check #(
+    .WIDTH(17),
+    .THRESHOLD(10000)
+)u_jump_lux(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.lux),
+    .temp_except(1'b0),
+    .jump_error(jump_error_lux)
+    );
+   
+    
+     noise_check #(
+    .WIDTH(17),
+    .HISTORY(10)
+)u_noise_lux(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_lux),
+    .new_sample(valid),
+    .noise_high(noise_high_lux)
+    );
+    
+ //humidity
+
+    range_check_signed #(
+    .WIDTH (8),
+    .THRESHOLD_MAX(100),
+    .THRESHOLD_MIN(0),
+    .USE_MIN(0),
+    .USE_MAX(1)
+    )u_range_humidity(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.humidity),
+    .out_in_range(out_in_range_hum)
+);
+   
+    jump_check #(
+    .WIDTH(8),
+    .THRESHOLD(10)
+)u_jump_humidity(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.humidity),
+    .temp_except(1'b0),
+    .jump_error(jump_error_hum)
+    );
+    
+     noise_check #(
+    .WIDTH(8),
+    .HISTORY(10)
+)u_noise_humidity(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_hum),
+    .new_sample(valid),
+    .noise_high(noise_high_hum)
+    );
+    
+//temperature
+//날씨 jumpe check 조건 따로 구현
+  
+  logic [1:0] prev_weather;
+  logic weather_change;
+  logic weather_change_next; //날씨가 변한 그 다음까지 예외처리해야되니까.
+  logic temp_except;
+  
+  always_ff @(posedge clk) begin //로직 따로 만들음
+    if(!rst_n)
+        prev_weather <= 2'b0;
+    else if (valid)
+        prev_weather <= sensor_data_in.weather; //weather 여기로 들어오는지 확인
+    end
+    
+    assign weather_change = (sensor_data_in.weather != prev_weather);
+    
+    always_ff @(posedge clk) begin
+        if(!rst_n)
+         weather_change_next <= 1'b0;
+    else if (valid)
+        weather_change_next <= weather_change;
+    end
+    
+    assign temp_except = weather_change || weather_change_next;
+
+    range_check_signed #(
+    .WIDTH (12),
+    .THRESHOLD_MAX(220),
+    .THRESHOLD_MIN(-30),
+    .USE_MIN(1),
+    .USE_MAX(1)
+    )u_range_temperature(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.temperature),
+    .out_in_range(out_in_range_temp)
+);
+   
+    jump_check #(    
+    .WIDTH(12),
+    .THRESHOLD(50)
+)u_jump_temperature(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.temperature),
+    .temp_except(temp_except),
+    .jump_error(jump_error_temp)
+    );
+    
+     noise_check #(
+    .WIDTH(12),
+    .HISTORY(10)
+)u_noise_temperature(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_temp),
+    .new_sample(valid),
+    .noise_high(noise_high_temp)
+    );
+
+//voltage
+//stuck check 빼도 되는거인가... ㅇㅇ
+
+    range_check_signed #(
+    .WIDTH (9),
+    .THRESHOLD_MAX(200),
+    .THRESHOLD_MIN(0),
+    .USE_MIN(0),
+    .USE_MAX(1)
+    )u_range_voltage(
+    .clk(clk),
+    .rst_n(rst_n),
+    .value(sensor_data_in.voltage),
+    .out_in_range(out_in_range_voltage)
+);
+   
+    jump_check #(
+    .WIDTH(9),
+    .THRESHOLD(20)
+)u_jump_voltage(
+    .clk(clk),
+    .rst_n(rst_n),
+    .current_data(sensor_data_in.voltage),
+    .temp_except(1'b0),
+    .jump_error(jump_error_voltage)
+    );
+    
+     noise_check #(
+    .WIDTH(9),
+    .HISTORY(10)
+)u_noise_voltage(
+    .clk(clk),
+    .rst_n(rst_n),
+    .jump_error(jump_error_voltage),
+    .new_sample(valid),
+    .noise_high(noise_high_voltage)
+    );
+
+//온도, 전압 경고
+    temp_voltage_checker u_temp_voltage_checker( 
+    .clk(clk),
+    .rst_n(rst_n),
+    .temperature(sensor_data_in.temperature),
+    .voltage(sensor_data_in.voltage),
+    .temperature_warn(temperature_warn),
+    .voltage_warn(voltage_warn)
+    );
+    
+
+//time-out_check
+    logic is_time_out;
+
+    timeout_check #(
+    .UPDATE_PERIOD(10),
+    .COUNTERWIDTH(10)
+    ) u_timeout_check (
+    .clk(clk),
+    .rst_n(rst_n),
+    .valid(valid),
+    .is_time_out(is_time_out)
+    );
+    
+    assign is_timeout_distance  = is_time_out;
+    assign is_timeout_appspeed  = is_time_out;
+    assign is_timeout_accel_x   = is_time_out;
+    assign is_timeout_accel_y   = is_time_out;
+    assign is_timeout_accel_z   = is_time_out;
+    assign is_timeout_gyro_x    = is_time_out;
+    assign is_timeout_gyro_y    = is_time_out;
+    assign is_timeout_gyro_z    = is_time_out;
+    assign is_timeout_lux       = is_time_out;
+    assign is_timeout_temp      = is_time_out;
+    assign is_timeout_hum       = is_time_out;
+    assign is_timeout_voltage   = is_time_out;
+    
+endmodule
+
+
